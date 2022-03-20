ec2db4e1cc2447b2731fcbc3d61403c389fb5007
Mark Snidal <mark.snidal@gmail.com>
2022-03-20 04:29:11 -0400

    Improve dataloader and add prelim neural stack

--- brazen-score/dataset.py
+++ brazen-score/dataset.py
@@ -1,58 +1,118 @@
 import os
 import pandas as pd
-import errno 
+import errno
+import pickle
 
+from pathlib import Path
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision.io import read_image, ImageReadMode
 from torchvision.transforms.functional import pad
 from pathlib import Path
 
-class PadToLargest(object):
-  def __init__(self, max_size=[0, 0]):
-    self.max_size = max_size
-    self.fill = 0
-    self.padding_mode = "constant"
-    
-  def __call__(self, tensor):
-    delta_width = self.max_size[0] - tensor.shape[2]
-    delta_height = self.max_size[1] - tensor.shape[1]
-    return pad(tensor, (0, 0, delta_width, delta_height))
+TOKEN_PATH = Path("token.pickle")
+
+
+class PadToLargest:
+    def __init__(self, max_image_size=[0, 0], fill=0):
+        self.max_image_size = max_image_size
+        self.fill = fill
+        self.padding_mode = "constant"
+
+    def __call__(self, tensor):
+        """Pad the given tensor on bottom and right sides to the max size with the given "pad" value"""
+        delta_width = self.max_image_size[0] - tensor.shape[2]
+        delta_height = self.max_image_size[1] - tensor.shape[1]
+        return pad(
+            tensor, (0, 0, delta_width, delta_height), self.fill, self.padding_mode
+        )
 
 
 class PrimusDataset(Dataset):
     def __init__(self, root_path: Path):
         if not root_path.exists():
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), root_path)
+
         self.root_path = root_path
         self.scores = [score.name for score in root_path.iterdir() if score.is_dir()]
-        self.max_size = [0, 0]
+        self.max_image_size = [2003, 288]  # self.get_max_image_size()
+        self.max_label_length = 75 # self.get_max_label_length()
 
-        """
-        for score in self.scores:
-            score_path = self.root_path / score / (score + ".png")
-            im = Image.open(str(score_path))
-            if im.size[0] > self.max_size[0]:
-                self.max_size[0] = im.size[0]
-            if im.size[1] > self.max_size[1]:
-                self.max_size[1] = im.size[1]
-        """
-        self.max_size = [2003, 288]
-            
-        self.transform = PadToLargest(self.max_size)
+        self.tokens = self.get_token_mapping()
+        self.transform = PadToLargest(self.max_image_size, 255)
 
     def __len__(self):
         return len(self.scores)
 
     def __getitem__(self, index):
-        score_path = self.root_path / self.scores[index]
-        image_file = score_path / (self.scores[index] + ".png")
-        image = read_image(str(image_file), ImageReadMode.GRAY)
-        text_file = score_path / (self.scores[index] + ".agnostic")
+        image = self.get_score_image(self.scores[index])
+        label = self.get_score_label(self.scores[index], encode_tokens=True, pad_length=True)
 
-        with text_file.open() as text:
-            label = text.read()
+        return image, label
+
+    def get_score_image(self, score):
+        """ Get the image for a score."""
+        image_file = self.root_path / score / (score + ".png")
+        image = read_image(str(image_file), ImageReadMode.GRAY)
 
         if self.transform:
             image = self.transform(image)
-        return image, label
+
+        return image
+
+    def get_score_label(self, score, encode_tokens=False, pad_length=False):
+        """ Get the vectorized, agnostic label for a score."""
+        agnostic_label_file = self.root_path / score / (score + ".agnostic")
+
+        with agnostic_label_file.open() as text:
+            raw_label = text.read()
+            label = [token for token in raw_label.split("\t") if token != ""]
+        
+        if encode_tokens:
+            label = [self.tokens.index(token) for token in label]
+        
+        if pad_length:
+            length_pad = [-1 for _ in range(self.max_label_length - len(label))]
+            label += length_pad
+
+        return label
+
+    def get_max_image_size(self):
+        """Get the maximum size of the score images."""
+        max_image_size = [0, 0]
+        for score in self.scores:
+            score_path = self.root_path / score / (score + ".png")
+            im = Image.open(str(score_path))
+            if im.size[0] > max_image_size[0]:
+                max_image_size[0] = im.size[0]
+            if im.size[1] > max_image_size[1]:
+                max_image_size[1] = im.size[1]
+        return max_image_size
+
+    def get_max_label_length(self):
+        """ Get the maximum length of the label sequence."""
+        max_length = 0
+        for score in self.scores:
+            label = self.get_score_label(score, encode_tokens=False)
+            if len(label) > max_length:
+                max_length = len(label)
+        return max_length
+
+    def get_token_mapping(self):
+        """Load or create the token mapping if it does not already exist. """
+
+        max_length = 0
+        if TOKEN_PATH.exists():
+            with open(str(TOKEN_PATH), "rb") as handle:
+                tokens = pickle.load(handle)
+        else:
+            tokens = []
+            for score in self.scores:
+                label = self.get_score_label(score, encode_tokens=False) # we can't do this until after this step
+                for token in label:
+                    if token not in tokens:
+                        tokens.append(token)
+
+            with open(str(TOKEN_PATH), "wb") as handle:
+                pickle.dump(tokens, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        return tokens
--- brazen-score/train.py
+++ brazen-score/train.py
@@ -1,15 +1,28 @@
-from dataset import PrimusDataset
-from matplotlib import pyplot as plt
 from pathlib import Path
-from torchvision import transforms
-import torch
+import collections
 
-primus_path = Path(Path.home(), Path("Data/sheet-music/primus"))
+from dataset import PrimusDataset
+from neural_stack import STACK
+
+BATCH_SIZE = 32
+PRIMUS_PATH = Path(Path.home(), Path("Data/sheet-music/primus"))
+
+
+from matplotlib import pyplot as plt
+from torch.utils import data as torchdata
+
 
 if __name__ == "__main__":
-  primus_dataset = PrimusDataset(primus_path)
+  primus_dataset = PrimusDataset(PRIMUS_PATH)
   score = primus_dataset[0]
   train_size = int(0.8 * len(primus_dataset))
   test_size = len(primus_dataset) - train_size
-  train_dataset, test_dataset = torch.utils.data.random_split(primus_dataset, [train_size, test_size])
-  print("DONE!")
+  train_dataset, test_dataset = torchdata.random_split(primus_dataset, [train_size, test_size])
+  train_loader = torchdata.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+  test_loader = torchdata.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+  train_length = len(train_dataset)
+  print("Train length:", train_length)
+  for index, (X, y) in enumerate(train_loader): #get index and batch
+    if index % 100 == 0:
+      print("Epoch done")
